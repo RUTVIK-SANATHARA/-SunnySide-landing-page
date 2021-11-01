3fe8f7943f63165fa23c133e74bf2728c387cd52
Rutvik sanathara <89308203+RUTVIK-SANATHARA@users.noreply.github.com>
2021-11-01 19:27:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("object");
+
+let toggleButton=document.getElementsByClassName('toggle-button')[0];
+let navLinks=document.getElementsByClassName('nav-links')[0];
+
+toggleButton.addEventListener('click',()=>{
+    navLinks.classList.toggle('active')
+    console.log("clicks")
+})
+
